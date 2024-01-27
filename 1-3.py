edade77a4ded825d48ca1687ec36cee57693dd52
Arshia-CdG <157867641+Arshia-CdG@users.noreply.github.com>
2024-01-27 09:22:14 +0330

    Update 1-3.py

--- 1-3.py
+++ 1-3.py
@@ -1,10 +1,12 @@
 b = bin(int(input()))[2:]
 a = bin(int(input()))[2:]
 s = [0] * 64
+
 for i in range(len(a)):
     s[i + 32 - len(a)] = a[i]
 for i in range(len(b)):
     s[i + 64 - len(b)] = b[i]
+
 m = int(input())
 s2 = [0] * m
 for i in range(m):
